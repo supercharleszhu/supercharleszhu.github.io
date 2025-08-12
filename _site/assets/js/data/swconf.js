3bc80a48d28c091118f8107c6d243ebaa88dc322
Chen <zhuchen1033@gmail.com>
2025-08-11 18:31:09 -0700

    update log Aug 11

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754611267',resources: [
+    cacheName: 'chirpy-1754962258',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
