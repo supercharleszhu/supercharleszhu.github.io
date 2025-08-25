e80d7f207f668f6be9355c3e59df54723703dd68
Chen <zhuchen1033@gmail.com>
2025-08-25 09:35:05 -0700

    update index

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754962258',resources: [
+    cacheName: 'chirpy-1756139674',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
